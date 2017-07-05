e4480feba39c5d7a48cc00daf0421dd145b43155
linqiwang <linqw@xinguangnet.com>
2017-07-05 11:01:03 +0800

    创建struts2

---
+++ Struts2/src/com/fmt/struct/HelloAction.java
@@ -0,0 +1,12 @@
+package com.fmt.struct;
+
+/**
+ * Created by 林其望 on 2017/7/5.
+ */
+public class HelloAction {
+
+    public String hello(){
+        System.out.println("hello");
+        return "success";
+    }
+}
