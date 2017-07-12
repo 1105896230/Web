f355f6dc9c8b2b1b0674165633f6deb2b922d4c1
fmt <1105896230@qq.com>
2017-07-12 15:49:49 +0800

    初始化项目成功

---
+++ bos/bos-web/src/main/java/com/fmt/bos/web/test.java
@@ -0,0 +1,11 @@
+package com.fmt.bos.web;
+
+import org.springframework.web.context.ContextLoaderListener;
+
+/**
+ * Created by 林其望 on 2017/7/12.
+ */
+public class test {
+    public static void main(String[] args){
+    }
+}
