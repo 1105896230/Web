73d34080f936949fcf965385a6d09800e51217ac
fmt <1105896230@qq.com>
2017-11-03 17:21:34 +0800

    用户模块

--- project/mmall/src/main/java/com/mmall/dao/UserMapper.java
+++ project/mmall/src/main/java/com/mmall/dao/UserMapper.java
@@ -16,9 +16,9 @@ public interface UserMapper {
 
     int updateByPrimaryKey(User record);
 
-    int checkUsername(String username);
+    int checkUsername(@Param("username") String username);
     int checkEmail(String email);
-    User selectLogin(@Param("username")String username,@Param("username")String password);
+    User selectLogin(@Param("username")String username,@Param("password")String password);
 
     String selectQuestionByUserName(String username);
 
